f1913a9cdafc68ecd1a3307d78b29e9b99d63fe2
udkfer <udkfer@gmail.com>
2026-05-03 21:29:37 -0300

    https://dmoj.ca/problem/aplusb

---
+++ DMOJ/A Plus B/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+
+int main() {
+  int n;
+  std::cin >> n;
+
+  for (int i = 0; i < n; i++) {
+    int a, b;
+    std::cin >> a >> b;
+    std::cout << a + b << '\n';
+  }
+}
+
