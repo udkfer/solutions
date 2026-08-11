d9a0ed6f1a0a716e3193c761320cba5b24ece6e0
udkfer <udkfer@gmail.com>
2026-08-11 11:43:17 -0300

    https://dmoj.ca/problem/ccc16j3

---
+++ DMOJ/CCC '16 J3 - Hidden Palindrome/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+
+bool isPalindrome(std::string s) {
+  int left{};
+  int right = s.length() - 1;
+
+  while (left < right) {
+    if (s[left] != s[right])
+      return false;
+
+    left++;
+    right--;
+  }
+
+  return true;
+}
+
+int main()
+{
+  std::string word{};
+  std::cin >> word;
+
+  int maxLen = 1;
+
+  for (int i{}; i < word.length(); i++) {
+    for (int j = i; j < word.length(); j++) {
+      std::string sub = word.substr(i, j - i + 1);
+
+      if (isPalindrome(sub) && sub.length() > maxLen) {
+        maxLen = sub.length();
+
+      }
+    }
+  }
+
+  std::cout << maxLen << '\n';
+
+  return 0;
+}
