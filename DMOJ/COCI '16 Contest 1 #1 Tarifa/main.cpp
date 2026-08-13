a0511b6c60aad7b88ed7e1404954f2ca23e226bc
udkfer <udkfer@gmail.com>
2026-08-13 12:23:19 -0300

    https://dmoj.ca/problem/coci16c1p1

---
+++ DMOJ/COCI '16 Contest 1 #1 Tarifa/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+  int x{}, n{};
+  std::cin >> x >> n;
+
+  int p{}, sum{};
+  for(int i{}; i < n; ++i)
+  {
+    std::cin >> p;
+    sum += p;
+  }
+
+  int surplus = ((n + 1) * x) - sum;
+
+  std::cout << surplus << '\n';
+  return 0;
+}
